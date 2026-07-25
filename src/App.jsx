9d703c84269854538614418b9fac07409b0466bd
Mantu Kumar Tiwari <mantu99739@gmail.com>
2026-07-25 16:04:23 +0530

    counter logic

--- src/App.jsx
+++ src/App.jsx
@@ -11,13 +11,14 @@ const App = () => {
     <div>
       <h1>Count: {count}</h1>
 
-      <button onClick={() => dispatch(increment())}>
-        Increase
-      </button>
-
-      <button onClick={() => dispatch(decrement())}>
-        Decrease
-      </button>
+      <div className="btn">
+        <button onClick={() => dispatch(decrement())}>
+          Decrease
+        </button>
+        <button onClick={() => dispatch(increment())}>
+          Increase
+        </button>
+      </div>
     </div>
   );
 };
