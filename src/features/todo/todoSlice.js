73b3bd1cc5591e5bedd063ae0ecf1ba2f6bd9bbd
Mantu Kumar Tiwari <mantu99739@gmail.com>
2026-07-25 16:25:30 +0530

    all slice created

--- src/features/todo/todoSlice.js
+++ src/features/todo/todoSlice.js
@@ -11,13 +11,33 @@ const todoSlice = createSlice({
     addTodo: (state, action) => {
       const newTodo = {
         id: Date.now(),
-        text: action.payload,
+        text: action.payload, // ye user input lega
         completed: false,
       };
 
       state.todos.push(newTodo);
     },
+
+    deleteTodo: (state, action) => {
+      state.todos = state.todos.filter((todo) => todo.id !== action.payload);
+    },
+
+    toggleComplete: (state, action) => {
+      const todo = state.todos.find((todo) => todo.id === action.payload);
+      if (todo) {
+        todo.completed = !todo.completed;
+      }
+    },
+
+    updateTodo: (state, action) => {
+      const { id, text } = action.payload;
+      const todo = state.todos.find((todo) => todo.id === id);
+      if (todo) {
+        todo.text = text;
+      }
+    },
   },
 });
 
+export const {addTodo, deleteTodo, toggleComplete, updateTodo} = todoSlice.actions;
 export default todoSlice.reducer;
