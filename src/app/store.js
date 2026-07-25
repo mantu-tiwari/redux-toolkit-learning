44cd8d339502025dc0add3f2add0dc4f8e4e5dc1
Mantu Kumar Tiwari <mantu99739@gmail.com>
2026-07-25 16:31:50 +0530

    store created

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
-import counterReducer from "../features/counterSlice";
+// import counterReducer from "../features/counterSlice";
+import todoReducer from "../features/counterSlice";
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
+    // counter: counterReducer,
+    todo : todoReducer,
   },
 });
